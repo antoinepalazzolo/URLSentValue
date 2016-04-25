d5a0e813c524b4b60e03de92180d69cc901a77f1
nataliapanferova <natalia.panferova.pro@gmail.com>
2016-04-25 16:52:44 +0200

    Add code

---
+++ URLSentValue.js
@@ -0,0 +1,73 @@
+var URLSentValue = function() {
+	this.evaluate = function() {
+		var exchange = this.req.getLastExchange();
+		var url = exchange.requestUrl;
+		var schema = "";
+		var domain = "";
+		var path = "";
+
+		var re = /^([^:]*:\/\/)([^\/]*)([^\?]*)?$/;
+		var m = re.exec(url);
+		if(m !== null) {
+			schema = m[1];
+			domain = m[2];
+			if(typeof m[3] !== 'undefined') {
+				path = m[3];
+			}
+
+		}
+
+		if (this.part === "schema") {
+			return schema;
+		}
+		else if (this.part === "domain") {
+			return domain;
+		}
+		else if (this.part === "path") {
+			return path;
+		}
+		else if (this.part === "schema_domain") {
+			return schema + domain;
+		}
+		else if (this.part === "domain_path") {
+			return domain + path;
+		}
+		else if (this.part === "schema_domain_path") {			
+			return schema + domain + path;
+		}
+		else {
+			return "";
+		}
+	}
+		
+
+
+	this.text = function(context) {
+		return this.req.name + " ➤ ";
+	}
+}
+
+URLSentValue.identifier = "com.luckymarmot.URLSentValue";
+
+URLSentValue.title = "URL Sent Value";
+
+URLSentValue.inputs = [
+	InputField("req", "Source Request", "Request"),
+	InputField(
+		"part",
+		"Part of the URL",
+		"Select", 
+		{"choices": 
+			{
+				"schema" : "schema",
+				"domain" : "domain",
+				"path" : "path",
+				"schema_domain" : "schema and domain",
+				"domain_path" : "domain and path",
+				"schema_domain_path": "schema, domain and path"
+			}
+		}
+	)
+]
+
+registerDynamicValueClass(URLSentValue);
